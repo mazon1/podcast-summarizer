522bf4b47aae80f8eb83d1a077396185c5073be9
Uchenna Mgbaja <dp_beryl@yahoo.com>
2023-08-15 15:56:29 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,52 +1,46 @@
 import streamlit as st
-import torchaudio
-from transformers import pipeline
+from pydub import AudioSegment
+import speech_recognition as sr
+import openai
+import tempfile
 
-# Load the Hugging Face model for ASR (Automatic Speech Recognition)
-asr = pipeline("automatic-speech-recognition")
+# Set your OpenAI API key
+#openai.api_key = st.secrets["openai_api_key"]
+openai.api_key = "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"
+
+def audio_to_text(audio_path):
+    sound = AudioSegment.from_mp3(audio_path)
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
+        audio_data = source.record(sound)
+        audio_text = r.recognize_google(audio_data=audio_data)
+    return audio_text
+
+def summarize_podcast(podcast_text):
+    prompt = f"Summarize the following podcast:\n{podcast_text}"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=150
+    )
+    summary = response.choices[0].text.strip()
+    return summary
 
 def main():
     st.title("Podcast Summarizer")
+    st.write("Upload a podcast audio file (in MP3 format) for summarization.")
+    uploaded_file = st.file_uploader("Choose an MP3 file", type="mp3")
 
-    uploaded_file = st.file_uploader("Upload a podcast audio file (M4A)", type=["m4a"])
+    if uploaded_file:
+        st.audio(uploaded_file, format='audio/mp3')
 
-    if uploaded_file is not None:
-        st.audio(uploaded_file, format="audio/m4a")
-
-        if st.button("Convert and Summarize"):
-            podcast_text = audio_to_text(uploaded_file)
-            st.subheader("Podcast Transcript:")
-            st.text(podcast_text)
-
-            # You can add your podcast summarization logic here
-            summarized_text = summarize_podcast(podcast_text)
-            st.subheader("Podcast Summary:")
-            st.text(summarized_text)
-
-def audio_to_text(uploaded_file):
-    try:
-        # Convert M4A to WAV and get ASR result
-        audio_path = "temp.wav"
-        with open(audio_path, "wb") as f:
-            f.write(uploaded_file.read())
-
-        # Load audio using torchaudio
-        waveform, sample_rate = torchaudio.load(audio_path)
-        
-        # Convert waveform to a compatible format for ASR
-        pcm_waveform = waveform.squeeze().numpy()
-
-        # Transcribe audio using Hugging Face ASR
-        asr_result = asr(pcm_waveform)
-        transcript = " ".join(asr_result)
-
-        return transcript
-    except Exception as e:
-        return f"Error: {e}"
-
-def summarize_podcast(text):
-    # Add your podcast summarization logic here
-    return "This is a summarized version of the podcast transcript."
+        if st.button("Summarize"):
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_file:
+                tmp_file.write(uploaded_file.read())
+            podcast_text = audio_to_text(tmp_file.name)
+            summary = summarize_podcast(podcast_text)
+            st.write("Podcast Summary:")
+            st.write(summary)
 
 if __name__ == "__main__":
     main()
