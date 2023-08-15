b315bcc1f3a389421a731d9d9fac7c8b9495ed2e
Uchenna Mgbaja <dp_beryl@yahoo.com>
2023-08-15 12:12:52 -0600

    Update openai-app.py

--- openai-app.py
+++ openai-app.py
@@ -1,22 +1,17 @@
-import openai
-from getpass import getpass
-
-openai.api_key = getpass('Enter the OpenAI API Key in the cell ')
-
 import streamlit as st
-import openai
 from pydub import AudioSegment
 import speech_recognition as sr
+import openai
 
 # Set your OpenAI API key
-#openai.api_key = "YOUR_OPENAI_API_KEY"
-openai.api_key = openai.api_key
+openai.api_key = st.secrets["openai_api_key"]
 
 def audio_to_text(audio_path):
     sound = AudioSegment.from_file(audio_path)
     r = sr.Recognizer()
     with sr.AudioFile(audio_path) as source:
-        audio_text = r.recognize_google(audio_data=source.record(sound))
+        audio_data = source.record(sound)
+        audio_text = r.recognize_google(audio_data=audio_data)
     return audio_text
 
 def summarize_podcast(podcast_text):
@@ -31,9 +26,7 @@ def summarize_podcast(podcast_text):
 
 def main():
     st.title("Podcast Summarizer")
-
     st.write("Upload a podcast audio file (in WAV format) for summarization.")
-
     uploaded_file = st.file_uploader("Choose a WAV file", type="wav")
 
     if uploaded_file:
@@ -47,4 +40,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
